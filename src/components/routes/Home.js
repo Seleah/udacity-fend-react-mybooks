5866058cbbdad16293efdfc359f90c1d5d325372
Seleah <seleah.face@gmail.com>
2018-10-08 13:44:17 -0700

    start building Home

---
+++ src/components/routes/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import * as BooksAPI from '../../BooksAPI';
+import Shelf from '../Shelf';
+
+
+class Home extends React.Component {
+
+}
+
+
+export default Home
