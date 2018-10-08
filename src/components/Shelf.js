da187ca3d4573fb39b63c420b74177209bbc9552
Seleah <seleah.face@gmail.com>
2018-10-08 15:08:40 -0700

    correct ul element in render function to make it an ol element className books-grid

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -8,11 +8,11 @@ class Shelf extends React.Component {
 			<div className="bookshelf">
 				<h2 className="bookshelf-title">{this.props.name}</h2>
 				<div className="bookshelf-books">
-					<ul className="books-display">
+					<ol className="books-grid">
 						{
 							this.props.books.map((book, key) => <Book updateBook={this.props.updateBook} book={book} key={key} />)
 						}
-					</ul>
+					</ol>
 				</div>
 			</div>
 		);
