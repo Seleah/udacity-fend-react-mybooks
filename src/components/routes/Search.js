1bce40e2435c4e65315d1f9cbe1bcf316459fd3b
Seleah <seleah.face@gmail.com>
2018-10-08 15:22:58 -0700

    write render() function in Search

--- src/components/routes/Search.js
+++ src/components/routes/Search.js
@@ -13,7 +13,21 @@ class Search extends React.Component {
 
 	render() {
 		return (
-			''
+			<div className="search-books">
+				<div className="search-books-bar">
+					<Link className="close-search" to="/">Close</Link>
+					<div className="search-books-input-wrapper">
+						<input type="text" placeholder="Search by title or author" value={this.state.query} onChange={(event) => this.updateQuery(event.target.value)} />
+					</div>
+				</div>
+				<div className="search-books-results">
+					<ol className="books-display">
+						{
+							this.state.results.map((book, key) => <Book updateBook={this.updateBook} book={book} key={key} />)
+						}
+					</ol>
+				</div>
+			</div>
 		);
 	}
 }
