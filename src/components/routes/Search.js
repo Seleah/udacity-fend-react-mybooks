daebccf67749acda482e8c8a01bc27705e0840b5
Seleah <seleah.face@gmail.com>
2018-10-08 15:20:50 -0700

    start writing Search component

---
+++ src/components/routes/Search.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import * as BooksAPI from '../../BooksAPI'
+import Book from '../Book.js'
+
+
+
+
+class Search extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			''
+		);
+	}
+}
+
+
+export default Search;
