d393a266ab6e6476d0f6a8d33cd8ff8ba67be975
Seleah <seleah.face@gmail.com>
2018-10-08 15:52:44 -0700

    add submitSearch() to Search

--- src/components/routes/Search.js
+++ src/components/routes/Search.js
@@ -23,6 +23,38 @@ class Search extends React.Component {
 		});
 	}
 
+	submitSearch() {
+		//  When there is nothing in the input field on the search page,
+		// show no results
+		if(this.state.query === '' || this.state.query === undefined) {
+			return this.setState({ results: [] });
+		}
+
+		//  If something is typed into the input field, attempt to search
+		//  and show results
+		BooksAPI.search(this.state.query.trim()).then(result => {
+			//  If there are no matches, show no matches
+			if(result.error) {
+				return this.setState({ results: [] });
+			} else {
+				// If there are matches, for every match, display the book
+				result.forEach(b => {
+					// Check each result (b) against the books you already have on a
+					// shelf. The point of a search is to ADD a book to your shelf, so
+					// filtering out the books already on a shelf from the list of results would be best.
+					let resultFiltered =  this.state.books.filter(B => B.id === b.id);
+					// Make sure that the shelf for each book matches the shelf it is on, if you've put it on a shelf
+
+					if(resultFiltered[0]) {
+						b.shelf = resultFiltered[0].shelf;
+						// console.log('before:', resultFiltered[0].shelf);
+					}
+				});
+				return this.setState({ results: result});
+			}
+		});
+	}
+
 	render() {
 		return (
 			<div className="search-books">
