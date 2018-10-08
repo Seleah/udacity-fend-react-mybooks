896b83b06240a3fd3047980865754c328819966c
Seleah <seleah.face@gmail.com>
2018-10-08 13:40:45 -0700

    fill shelf with book components

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -5,7 +5,16 @@ import Book from './Book.js';
 class Shelf extends React.Component {
 	render() {
 		return (
-			''
+			<div className="bookshelf">
+				<h2 className="bookshelf-title">{this.props.name}</h2>
+				<div className="bookshelf-books">
+					<ul className="books-display">
+						{
+							this.props.books.map((book, key) => <Book updateBook={this.props.updateBook} book={book} key={key} />)
+						}
+					</ul>
+				</div>
+			</div>
 		);
 	}
 }
