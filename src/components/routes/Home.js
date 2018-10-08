8717997400c92612ab06e3925b5ec488b66c8723
Seleah <seleah.face@gmail.com>
2018-10-08 15:14:31 -0700

    finish Home

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -20,7 +20,7 @@ class Home extends React.Component {
 	}
 
 	updateBook = (book, shelf) => {
-		BooksAPI.update(book, shelf);
+		BooksAPI.update(book, shelf)
 		.then(response => {
 			book.shelf = shelf;
 			this.setState(state => ({
@@ -31,7 +31,21 @@ class Home extends React.Component {
 
 	render() {
 		return (
-			''
+			<div className="list-books">
+				<div className="list-books-title">
+					<h1>MyReads</h1>
+				</div>
+				<div className="list-books-content">
+					<div>
+						<Shelf updateBook={this.updateBook} name="Currently Reading" books={this.state.books.filter(b => b.shelf === "currentlyReading")} />
+						<Shelf updateBook={this.updateBook} name="Want To Read" books={this.state.books.filter(b => b.shelf === "wantToRead")} />
+						<Shelf updateBook={this.updateBook} name="Read" books={this.state.books.filter(b => b.shelf === "read")} />
+					</div>
+				</div>
+				<div className="open-search">
+					<Link to="/search">Add a book</Link>
+				</div>
+			</div>
 		);
 	}
 }
